5460cc61c1ba92f1b981a824b0f0c409a7e00661
prospectdanik <prospectdanik@mail.ru>
2023-06-18 17:39:53 +0600

    Занятие 2

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
-console.log("Hello" + " World");
-console.log(222);
+console.log('Добро ' + 'пожаловать' + ' на курс' + ' Daniyar');
+console.log('9 may');
+
+// alert('18.06.2023')
+
+// document.querySelector(".one").innerHTML - "Это наш первый вывод по средствам JS";
+
+
+// console.log(1 / 0);
+// console.log(9 * "qwe");
+// console.log(1<2);
+// console.log(null);
+console.log(typeof "big");
+console.log(typeof 20);
+console.log(typeof null);
