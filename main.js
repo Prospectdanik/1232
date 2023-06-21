18a4064afe65c68cfeadb9fd9d55ac7c5b400276
prospectdanik <prospectdanik@mail.ru>
2023-06-21 17:07:05 +0600

    main.js

--- main.js
+++ main.js
@@ -3,13 +3,32 @@ console.log('9 may');
 
 // alert('18.06.2023')
 
-// document.querySelector(".one").innerHTML - "Это наш первый вывод по средствам JS";
-
+// document.querySelector(".one").innerHTML = "Это наш первый вывод по средствам JS";
 
 // console.log(1 / 0);
 // console.log(9 * "qwe");
 // console.log(1<2);
 // console.log(null);
-console.log(typeof "big");
-console.log(typeof 20);
-console.log(typeof null);
+// console.log(typeof "big");
+// console.log(typeof 20);
+// console.log(typeof null);
+
+// document.getElementById("two").innerHTML = "Abiev";
+
+// let inp = document.querySelector(".text");
+// let btn = document.querySelector(".check");
+// let div = document.querySelector("div");
+
+// btn.onclick = function () {
+// let e = inp.value;
+// div.innerHTML = `<h1>${e}</h1>`;
+// };
+
+let dr = prompt("Когда у тебя ДР?");
+console.log(dr);
+if (dr > 9) {
+    alert("С днем рождения дорогой")
+} else {
+    alert("Приходи на завтра")
+}
+
